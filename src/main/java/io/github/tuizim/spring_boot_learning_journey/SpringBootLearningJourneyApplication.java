4ff9aa6db9a2083e06e8bf5408e14d4b9fc100fc
Tuizim <tuitrombim@gmail.com>
2025-01-14 10:21:27 -0300

    First Commit

---
+++ src/main/java/io/github/tuizim/spring_boot_learning_journey/SpringBootLearningJourneyApplication.java
@@ -0,0 +1,13 @@
+package io.github.tuizim.spring_boot_learning_journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tuizim/spring_boot_learning_journey/SpringBootLearningJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tuizim.spring_boot_learning_journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
